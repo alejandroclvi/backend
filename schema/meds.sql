928a3ae350fe95298f06ee1ec9aafd25993e077d
Manuel Calvino <mcalvinolaguardia@gmail.com>
2020-08-25 18:42:57 -0400

    updated schema

--- schema/meds.sql
+++ schema/meds.sql
@@ -2,12 +2,11 @@
 
 create table bravado_db.meds (
   id                   serial primary key,
-  dosis                text,
+  dosis                integer not null default 0,
   frequency            text,
   cost                 float,
   user_id              integer references bravado_db.user (id) on delete cascade,
   name                 text not null check (char_length(name) < 80)
 );
 
-create unique index on bravado_db.meds (name);
 create unique index on bravado_db.meds (id);
